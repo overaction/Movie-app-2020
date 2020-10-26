9a337ebeda902d79799f7ec2fd93822abfad2fbf
overaction <52850392+overaction@users.noreply.github.com>
2020-10-26 23:56:27 +0900

    6. Add default image
    default image is overaction rabbit

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -3,7 +3,7 @@ const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
 
 const Movie = ({ title, poster_path, overview, vote_average, setVote}) => (
   <div className="movie">
-    <img src={IMG_PATH + poster_path} alt={title} />
+    <img src={(poster_path ? IMG_PATH +poster_path : "https://pht.qoo-static.com/HQ_yGW5ZTPFilafXKljCV-s3i3ZWsQpaCX02Te1WPNzdZyKhGSAzixV8CihsFlrNTaE=w300-rw")} alt={title} />
     <div className="movie-info">
       <h3>{title}</h3>
       <span className={`tag ${setVote(vote_average)}`}>{vote_average}</span>
