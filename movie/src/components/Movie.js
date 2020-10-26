4c8637f5960c73f204d42cd89a751a6e86cc14f4
overaction <52850392+overaction@users.noreply.github.com>
2020-10-26 16:33:59 +0900

    3. movie overview, css

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -1,14 +1,18 @@
 import React from 'react';
 const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
 
-const Movie = ({title, poster_path, overview, vote_average}) => (
-    <div className="movie">
-        <img src={IMG_PATH + poster_path} alt={title} />
-        <div className="movie-info">
-            <h3>{title}</h3>
-            <span>{vote_average}</span>
-        </div>
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img src={IMG_PATH + poster_path} alt={title} />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
     </div>
+    <div className="movie-over">
+      <h2>{title}</h2>
+      <p>{overview}</p>
+    </div>
+  </div>
 );
 
 export default Movie;
